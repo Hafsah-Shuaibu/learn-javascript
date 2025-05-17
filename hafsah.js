0be607e1d3503a7350d50ab0ae0d0682a529750a
Hafsah-Shuaibu <hashcy01@gmail.com>
2025-05-17 22:49:33 +0100

    practiced using comparison operators

--- hafsah.js
+++ hafsah.js
@@ -21,3 +21,36 @@ console.log(9/4);
 //multiplication (*)
 
 console.log(9 * 4);
+
+
+
+
+//Comparison operators
+
+let h = 8
+
+
+//equal to (==)
+
+console.log(h == 8); // result was true
+console.log(h == 6); // result was false
+
+
+
+// equal value and equal type (===)
+
+console.log(h === 8); // result was true
+console.log(h === "8"); //result was false
+
+
+// not equal (!=)
+
+console.log(h != 8); // result was false
+console.log(h != 7); // result was true
+
+// not equal value or not equal type (!==)
+
+console.log(h !== 8); //result was false
+console.log(h !== 7); //result was true
+
+
