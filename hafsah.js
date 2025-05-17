b51a8999cad432b168f72bf09ecbecd32986a99f
Hafsah-Shuaibu <hashcy01@gmail.com>
2025-05-17 22:31:30 +0100

    practiced using arithmetic operators

---
+++ hafsah.js
@@ -0,0 +1,23 @@
+// arithmetic operators
+
+
+
+//Addition (+)
+
+console.log(4 + 9);
+
+
+
+//subtraction (-)
+
+console.log(9 - 4);
+
+
+//division (/)
+
+console.log(9/4);
+
+
+//multiplication (*)
+
+console.log(9 * 4);
