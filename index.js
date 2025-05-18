a90640f336a52a5748b2dd98229aaae930ffceb9
Hafsah-Shuaibu <hashcy01@gmail.com>
2025-05-18 21:45:18 +0100

    wrote basic conditional code

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 let age = 18;
 
 if(age >= 18) {
-    console.log("You are welcome")
+    console.log("You are welcome");
 }
 
 //else stetment
@@ -13,9 +13,9 @@ if(age >= 18) {
 let height = 7;
 
 if(height >= 18) {
-    console.log("You are welcome")
+    console.log("You are welcome");
 } else{
-    console.log("Leave")
+    console.log("Leave");
 }
 
 // else if statement
@@ -23,9 +23,22 @@ if(height >= 18) {
 let weight = 28;
 
 if(weight >= 70) { 
-    console.log ("hello")
+    console.log ("hello");
 } else if(weight < 30) {
-    console.log("Hi")
-} else{ console.log ("Hey")
+    console.log("Hi");
+} else{ console.log ("Hey");
 
+}
+
+//writing basic conditional code
+
+let heat = 30;
+
+if(heat > 35) {
+    console.log("it's very hot outside");
+}
+else if(temperature >= 20) {
+    console.log("The weather is pleasant");
+}
+else{ console.log("it's cold outside");
 }
