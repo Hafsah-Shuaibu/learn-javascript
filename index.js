25cd474361f60772581afe7a7b4a6d8680a6af8f
Hafsah-Shuaibu <hashcy01@gmail.com>
2025-05-18 21:25:25 +0100

    practiced using if statement and else statement

---
+++ index.js
@@ -0,0 +1,19 @@
+// conditional statement
+
+// if statement
+
+let age = 18
+
+if(age >= 18) {
+    console.log("You are welcome")
+}
+
+//else stetment
+
+let height = 7
+
+if(height >= 18) {
+    console.log("You are welcome")
+} else{
+    console.log("Leave")
+}
