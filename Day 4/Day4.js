3fc857394ae3cf4e9101262ea70cdda9c449f6ed
Hafsah-Shuaibu <hashcy01@gmail.com>
2025-05-20 00:08:23 +0100

    re-practiced using for statement

--- Day 4/Day4.js
+++ Day 4/Day4.js
@@ -2,3 +2,10 @@ for (let i = 0; i < 5; i++) {
    console.log(" hi hafsah");
 }
 
+for (let i = 4; i < 14; i++) { 
+    console.log ("hafsah is working"); 
+}
+
+for (i = 9; i < 29; i++) {
+    console.log("how're you today?");
+}
