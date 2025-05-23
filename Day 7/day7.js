3ca4e23a9e6551a1c455fab626142f944302e718
Hafsah-Shuaibu <hashcy01@gmail.com>
2025-05-23 01:02:37 +0100

    created a function with default parameters

--- Day 7/day7.js
+++ Day 7/day7.js
@@ -1,5 +1,11 @@
 // without default parameter
 
 function hafsah(name, gender, age){
-    console.log("my name is" + name + "i am a" + gender + "and i am" + age + "years old")
-}
+    console.log("my name is" + name + "i am a" + gender + "and i am" + age + "years old");
+} hafsah ()
+
+// with default parameter
+
+function hafsah(name = "hafsah", gender = "female", age = 25){
+    console.log("my name is " + name + " i am a " + gender + " and i am " + age + " years old");
+} hafsah()
