cf112abea2b32e635b2b7bc6c1e2a0f57c709bc9
Hafsah-Shuaibu <hashcy01@gmail.com>
2025-05-23 01:21:14 +0100

    practiced rest parameter in a function

--- Day 7/day7.js
+++ Day 7/day7.js
@@ -9,3 +9,15 @@ function hafsah(name, gender, age){
 function hafsah(name = "hafsah", gender = "female", age = 25){
     console.log("my name is " + name + " i am a " + gender + " and i am " + age + " years old");
 } hafsah()
+
+// rest parameter
+
+function kitchen(...items) {
+    console.log(items)
+}
+
+const item1 = "cutter";
+const item2 = "pot";
+const item3 = "fridge";
+
+kitchen(item1, item2, item3);
