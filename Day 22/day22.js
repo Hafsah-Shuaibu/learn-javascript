ae0542da19da5d9380acf6cdd818f80c3c09ba49
Hafsah-Shuaibu <hashcy01@gmail.com>
2025-06-11 04:49:36 +0100

    practiced selecting elements using tag name

--- Day 22/day22.js
+++ Day 22/day22.js
@@ -3,8 +3,14 @@
 const greeting = document.getElementById("greeting");
 greeting.style.background = "purple"
 
-//Using getElementsClassname()
+//Using getElementsByClassname()
 
 const foods = document.getElementsByClassName("foods");
 foods[0].style.backgroundColor = "red"
 foods[2].style.backgroundColor = "green"
+
+//Using getElementsByTagName()
+
+const h2Tags = document.getElementsByTagName("h2");
+h2Tags[0].style.background = "blue"
+h2Tags[2].style.fontSize = "50px"
