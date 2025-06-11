5baf441fec3066002a73102f091733a52e225ea8
Hafsah-Shuaibu <hashcy01@gmail.com>
2025-06-11 04:40:37 +0100

    practiced selecting elements using class name

--- Day 22/day22.js
+++ Day 22/day22.js
@@ -5,3 +5,6 @@ greeting.style.background = "purple"
 
 //Using getElementsClassname()
 
+const foods = document.getElementsByClassName("foods");
+foods[0].style.backgroundColor = "red"
+foods[2].style.backgroundColor = "green"
